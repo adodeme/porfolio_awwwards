f3491833e258b9c981f05b07d6b48e2fd5e642e2
adodeme <adodemeflodias@gmail.com>
2026-05-24 04:58:12 +0100

    Mouse reactive sphere

--- components/SpaceBackground.tsx
+++ components/SpaceBackground.tsx
@@ -8,17 +8,33 @@ import * as THREE from "three";
 function EnergySphere() {
   const mesh = useRef<THREE.Mesh>(null!);
 
-  useFrame((_, delta) => {
+  useFrame(({ mouse }, delta) => {
+    if (!mesh.current) return;
+
     mesh.current.rotation.y += delta * 0.5;
     mesh.current.rotation.x += delta * 0.2;
+
+    mesh.current.position.x = THREE.MathUtils.lerp(
+      mesh.current.position.x,
+      mouse.x * 1.5,
+      0.05
+    );
+
+    mesh.current.position.y = THREE.MathUtils.lerp(
+      mesh.current.position.y,
+      mouse.y * 1.5,
+      0.05
+    );
   });
 
   return (
-    <Float speed={2} rotationIntensity={2}>
+    <Float speed={2} rotationIntensity={2} floatIntensity={2}>
       <mesh ref={mesh}>
-        <icosahedronGeometry args={[1.5, 2]} />
+        <icosahedronGeometry args={[1.5, 3]} />
         <meshStandardMaterial
           color="#00ffff"
+          emissive="#00ffff"
+          emissiveIntensity={2}
           wireframe
         />
       </mesh>
@@ -31,7 +47,8 @@ export default function SpaceBackground() {
     <div className="absolute inset-0">
       <Canvas camera={{ position: [0, 0, 5] }}>
         <ambientLight intensity={1} />
-        <pointLight position={[5, 5, 5]} />
+        <pointLight position={[5, 5, 5]} intensity={10} />
+        <pointLight position={[-5, -5, -5]} intensity={5} />
         <EnergySphere />
       </Canvas>
     </div>
